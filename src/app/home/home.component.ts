3eeb4d0188a1f610c94926bd3c6bb13fd11bd14b
Thendo Ramashia <kramashia101@gmail.com>
2023-10-01 00:32:56 +0200

    added logic for loading products and expanding for all

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Product } from './product.model';
+import { ProductService } from '../service/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,13 +9,41 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
+
+  products!: Product[];
+  filteredProducts: Product[] = [];
+  categories: string[] = [];
+  selectedCategory = '';
+  searchKeyword = '';
+  showAll = false;
+  totalProducts: number = 0;
+  productSub!: Subscription;
+
+  constructor(private productService: ProductService) {}
   
   ngOnInit(): void {
-    
+    this.loadProducts();
   }
-  
+
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+    this.productSub.unsubscribe();
+  }
+
+  loadProducts() {
+    this.productSub = this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.totalProducts = this.products.length;
+        console.table(this.products);
+        this.filteredProducts = this.products.slice(0, 8); 
+        this.categories = [...new Set(this.products.map(product => product.category))];
+      }
+    })
+  }
+
+  expandList(): void {
+    this.showAll = true;
+    this.filteredProducts = [...this.products];
   }
   
 }
